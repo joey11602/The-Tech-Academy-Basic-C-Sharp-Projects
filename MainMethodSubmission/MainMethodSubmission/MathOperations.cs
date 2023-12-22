c88eed51140afc1deddd52a6f8f085b8409d32e8
joey11602 <joey35475@outlook.com>
2023-12-21 21:53:09 -0600

    upload for submission

---
+++ MainMethodSubmission/MainMethodSubmission/MathOperations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class MathOperations //create a class called Mathoperations
+    {
+        public int PerformMathOperation(int number) //method for integer parameter
+        {
+            int result = number + 5; //add 5 to the parameter number
+            return result; //return result to main program
+        }
+
+        public int PerformMathOperation(decimal decimalNumber) //method for decimal parameter
+        {
+            int result = (int)(decimalNumber * 2); //multiply the parameter decimalNumber by 2 and convert the answer to an integer
+            return result; //return result to main program
+        }
+
+        public int PerformMathOperation(string strNumber) //method for string parameter
+        {
+            int parsedNumber; //define variable parsedNumber
+            if (int.TryParse(strNumber, out parsedNumber)) ; //use TryParse to convert string to integer
+            else //if TryParse fails use 0
+            {
+                parsedNumber = 0;
+            }
+            int result = parsedNumber * parsedNumber; //square the parameter strNumber
+            return result; //return result to main program
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathOperations mathOperations = new MathOperations(); //instantiate the MathOperations class
+
+            int intResult = mathOperations.PerformMathOperation(10); //call the first method with an integer
+            Console.WriteLine($"Result for integer parameter: {intResult}"); //write to console the result
+
+            int decimalResult = mathOperations.PerformMathOperation(7.5M); //call the second method with a decimal
+            Console.WriteLine($"Result for decimal parameter: {decimalResult}"); //write to console the result
+
+            int stringResult = mathOperations.PerformMathOperation("15"); //call the third method with a string that can be converted to an integer
+            Console.WriteLine($"Result for string parameter: {stringResult}"); //write to console the result
+            Console.ReadLine(); //hold console open
+        }
+        
+    }
+}
