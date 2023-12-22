6372484821045bac0f77fc1d7ac80b4d2843ede7
joey11602 <joey35475@outlook.com>
2023-12-21 21:56:29 -0600

    removed unnecessary lines

--- MainMethodSubmission/MainMethodSubmission/MathOperations.cs
+++ MainMethodSubmission/MainMethodSubmission/MathOperations.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MainMethodSubmission
+﻿namespace MainMethodSubmission
 {
     class MathOperations //create a class called Mathoperations
     {
