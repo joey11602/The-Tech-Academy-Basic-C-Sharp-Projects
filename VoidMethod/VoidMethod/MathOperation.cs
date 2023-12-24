acff801e5678437080d03e5515face828ac4ab53
joey11602 <joey35475@outlook.com>
2023-12-24 15:53:05 -0600

    upload for submission

---
+++ VoidMethod/VoidMethod/MathOperation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class MathOperation //create a class
+    {
+        public void PerformOperation(int firstNumber, int secondNumber) //create a void method that takes two parameters
+        {
+            int result = firstNumber * 2; //perform a math operation on the first parameter and assign it to int variable result
+                                          //(***does not say to display this***)
+            Console.WriteLine($"Second Number: {secondNumber}"); //display the second parameter to the console screen 
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main()
+        {            
+            MathOperation mathOperation = new MathOperation(); //instantiate the MathOperation class
+                                                               //
+            mathOperation.PerformOperation(3, 5); //call the PerformOperation method and pass in two parameters
+
+            mathOperation.PerformOperation(firstNumber: 7, secondNumber: 10); //call the PerformOperation method and pass in two parameters by name
+
+            Console.ReadLine(); //hold console open
+        }
+    }
+}
