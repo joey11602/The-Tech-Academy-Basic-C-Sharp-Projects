8745e4d30652bb5c588ae7aad3a30da8e49364bd
joey11602 <joey35475@outlook.com>
2023-12-31 20:10:07 -0600

    upload to GitHub

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public struct Number //create a struct called 'Number'
+    {
+        public decimal Amount { get; set; } //gets or sets the Amount of the Number struct
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number number = new Number { Amount = 5.50m }; //create an instance of the Number struct and assigm its property Amount a value
+            Console.WriteLine($"The amount is: {number.Amount}"); //write to the console the amount
+            Console.ReadLine(); //hold open the console
+        }
+    }
+}
