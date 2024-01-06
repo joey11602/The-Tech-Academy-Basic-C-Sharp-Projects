d996c1b09dd8dc6fc0b97ad4668cc2ca66da3215
joey11602 <joey35475@outlook.com>
2024-01-05 23:10:48 -0600

    upload for submission

---
+++ ConstructorSubmission/ConstructorSubmission/Person.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ConstructorSubmission
+{
+    //create the class Person
+    public class Person
+    {
+        //constructor with the FirstName parameter that chains to the second constructor by providing the default Age value
+        public Person(string firstName) : this(firstName, 21)
+        {
+        }
+        //constructor with the FirstName and Age parameters
+        public Person(string firstName, int age)
+        {
+            //assign values to FirstName and Age
+            FirstName = firstName;
+            Age = age;
+        }
+        //gets or sets the first name of the person
+        public string FirstName { get; set; }
+        //gets or sets the age of the person
+        public int Age { get; set; }
+    }
+}
---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            //create a const variable
+            const string constVariable = " is my name and I'm older than ";
+
+            //instantiate an object of Person class by using the keyword var for my variable
+            var newPerson = new Person("Joey");
+
+            //write to console
+            Console.WriteLine(newPerson.FirstName + constVariable + newPerson.Age + ".");
+            //hold console open
+            Console.ReadLine();
+            
+        }
+    }
+}
